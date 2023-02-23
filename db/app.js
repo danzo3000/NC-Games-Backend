0bde45fe76bb01584621aa6fb4d7211d9323950e
danzo3000 <dbowman9213@gmail.com>
2023-02-23 09:44:28 +0000

    adds final 400 tests and working post request at /api/reviews/:review_id/comments

--- db/app.js
+++ db/app.js
@@ -1,28 +1,32 @@
-const express = require('express');
-const { handle500Errors, handle400Errors, handleCustomErrors} = require('./controllers/errorHandlingControllers');
+const express = require("express");
+const {
+  handle500Errors,
+  handle400Errors,
+  handleCustomErrors,
+} = require("./controllers/errorHandlingControllers");
 const app = express();
 const {
-    getCategories,
-    getReviews,
-    getReviewByID,
-    getCommentsByReviewID,
-    postCommentByReviewID
-} = require('./controllers/gamesControllers');
+  getCategories,
+  getReviews,
+  getReviewByID,
+  getCommentsByReviewID,
+  postCommentByReviewID,
+} = require("./controllers/gamesControllers");
 
 app.use(express.json());
 
-app.get('/api/categories', getCategories)
+app.get("/api/categories", getCategories);
 
-app.get('/api/reviews', getReviews)
+app.get("/api/reviews", getReviews);
 
-app.get('/api/reviews/:review_id', getReviewByID)
+app.get("/api/reviews/:review_id", getReviewByID);
 
-app.get('/api/reviews/:review_id/comments', getCommentsByReviewID)
+app.get("/api/reviews/:review_id/comments", getCommentsByReviewID);
 
-app.post('/api/reviews/:review_id/comments', postCommentByReviewID)
+app.post("/api/reviews/:review_id/comments", postCommentByReviewID);
 
-app.use(handleCustomErrors)
-app.use(handle400Errors)
-app.use(handle500Errors)
+app.use(handleCustomErrors);
+app.use(handle400Errors);
+app.use(handle500Errors);
 
-module.exports = app
+module.exports = app;
--- db/controllers/errorHandlingControllers.js
+++ db/controllers/errorHandlingControllers.js
@@ -1,22 +1,19 @@
-exports.handle500Errors = (err, req, res, next)=> {
-    console.log(err);
-    res.status(500).send({msg: "Server error"})
-}
+exports.handle500Errors = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "Server error" });
+};
 
 exports.handle400Errors = (err, req, res, next) => {
-    
-    if (err.code === "22P02") {
-        res.status(400).send({msg: "Bad Request"})
-    }
-    else {
-        next(err)
-    }
-}
+  if (err.code === "22P02" || err.code === "23503") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else {
+    next(err);
+  }
+};
 
 exports.handleCustomErrors = (err, req, res, next) => {
-    if (err.status && err.msg) {
-        res.status(err.status).send({msg: err.msg})
-    }
-    next(err)
-}
-
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+  next(err);
+};
